52e2253f85fa430b69973e6e11b79f88f3c2ddfb
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-05 10:39:31 -0600

    adding expense items

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>January 5th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$400.00</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
