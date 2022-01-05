fdb41ae2833fcdd2cabfcedf3e75dfeaeda7ed33
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-05 10:49:09 -0600

    adding basic css style

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>January 5th 2022</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$400.00</div>
+        <div className="expense-item__price">$400.00</div>
       </div>
     </div>
   );
