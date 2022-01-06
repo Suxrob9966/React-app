b7b1ea197ba7b2b8b02c8d310fd6bceea4aa4f47
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-06 13:21:35 -0600

    adding event listeners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,11 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 import React from "react";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  function clickHandler() {
+    console.log("Clicked!!");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,8 +15,9 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
