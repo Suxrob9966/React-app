f3d4f4c889535ac4560f36bae358f0fffc7dc49a
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-09 09:47:05 -0600

    stateful vs stateless statements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,21 @@ import "./ExpenseItem.css";
 import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title); // must be called directly inside component function. It always returns an array with exactly 2 elements. First element is current state value, second is the function to update that value
+  // const [title, setTitle] = useState(props.title); // must be called directly inside component function. It always returns an array with exactly 2 elements. First element is current state value, second is the function to update that value
 
-  function clickHandler() {
-    setTitle("Updated!"); // pass new value to title
-    console.log("Clicked!!");
-  }
+  // function clickHandler() { // this is for demo purposes. needs to be commented
+  //   setTitle("Updated!"); // pass new value to title
+  //   console.log("Clicked!!");
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 };
