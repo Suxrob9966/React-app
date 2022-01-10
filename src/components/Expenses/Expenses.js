ffa7c3abd8788c57ef000566736a0dc9c7cfd9d8
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-10 11:43:18 -0600

    finished chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 import React, { useState } from "react";
 
@@ -22,6 +23,7 @@ const Expenses = (props) => {
           onFilterExpense={expenseFilterHandler}
           selected={filteredYear}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
